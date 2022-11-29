90c78fa0ca78cb96dd203a87903a08391cab1178
Ori M. K <kosman.ori@gmail.com>
2022-11-29 16:24:10 +0200

    Added REST API functionality and connection to DB

--- server/controllers/vaultController.cjs
+++ server/controllers/vaultController.cjs
@@ -1,17 +1,27 @@
 const asyncHandler = require('express-async-handler');
+const Vault = require('../models/vaultModel.cjs');
 
 // @desc    Get all passwords
 // @route   GET /api/vault
 // @access  Private
 const getPasswords = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Get passwords' });
+	const passwords = await Vault.find();
+	res.status(200).json(passwords);
 });
 
 // @desc    Get a password
 // @route   GET /api/vault/:id
 // @access  Private
 const getPassword = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Get password ${req.params.id}` });
+	const pwdId = req.params.id;
+	const password = await Vault.findById(pwdId);
+
+	if (!password) {
+		res.status(400);
+		throw new Error(`Password with ID: ${id} does not exist`);
+	}
+
+	res.status(200).json(password);
 });
 
 // @desc    Set a password
@@ -22,21 +32,47 @@ const setPassword = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error('Please use a text field');
 	}
-	res.status(200).json({ message: 'Set Password' });
+
+	const password = await Vault.create({
+		text: req.body.text,
+	});
+	res.status(200).json(password);
 });
 
 // @desc    Update password
 // @route   PUT /api/vault/:id
 // @access  Private
 const updatePassword = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Update password ${req.params.id}` });
+	const id = req.params.id;
+	const password = await Vault.findById(id);
+
+	if (!password) {
+		res.status(400);
+		throw new Error(`Password with ID: ${id} does not exist`);
+	}
+
+	const updatedPwd = await Vault.findByIdAndUpdate(id, req.body, { new: true });
+
+	res.status(200).json(updatedPwd);
 });
 
 // @desc    Delete password
 // @route   DELETE /api/vault
 // @access  Private
 const deletePassword = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Delete password ${req.params.id}` });
+	const pwdId = req.params.id;
+	const password = await Vault.findById(pwdId);
+
+	if (!password) {
+		res.status(400);
+		throw new Error(`Password with ID: ${pwdId} does not exist`);
+	}
+
+	// const passwordToDelete = await Vault.findByIdAndDelete(id);
+	await password.remove();
+
+	// res.status(200).json(passwordToDelete);
+	res.status(200).json({ id: pwdId });
 });
 
 module.exports = {
--- server/server.cjs
+++ server/server.cjs
@@ -1,15 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db.cjs');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware.cjs');
-
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/vault', require('./routes/vaultRoutes.cjs'));
-
 app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
