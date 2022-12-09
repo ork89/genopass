4b1e070f20ba5ea40557dc1920f9e0d753e789cf
ork89 <kosman.ori@gmail.com>
2022-12-09 23:49:06 +0200

    prepared for deployment

--- server/server.cjs
+++ server/server.cjs
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db.cjs');
 const dotenv = require('dotenv').config();
@@ -12,6 +13,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/vault', require('./routes/vaultRoutes.cjs'));
 app.use('/api/users', require('./routes/userRoutes.cjs'));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/dist')));
+
+	app.get('*', (response, request) =>
+		response.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'))
+	);
+} else {
+	app.get('/', (request, response) => response.send('Environment is not set to production'));
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
